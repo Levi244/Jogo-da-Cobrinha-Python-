de66e703d35877018c0266b9d3510ab32585d72e
Levi <levirs999@gmail.com>
2025-12-15 21:42:33 -0300

    Jogo base

---
+++ main.py
@@ -0,0 +1,46 @@
+
+import pygame
+import random
+
+pygame.init()
+pygame.display.set_caption("Jogo da Cobrinha em Python")
+largura, altura = 1200, 800
+tela = pygame.display.set_mode((largura, altura))
+relogio = pygame.time.Clock()
+
+# Define cores
+preto = (0, 0, 0)
+branco = (255, 255, 255)
+azul = (0, 0, 255)
+verde = (0, 255, 0)
+
+# Definir parametros
+tamanho_quadrado = 15
+velocidade_cobra = 15
+
+def rodar_jogo():
+    fim_jogo = False
+
+    x = largura / 2
+    y = altura / 2 
+
+    velocidade_x = 0
+    velocidade_y = 0
+
+
+    tamanho_cobra = 1
+    pixels_cobra = []
+
+    while not fim_jogo:
+        tela.fill(preto)    
+
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                fim_jogo = True
+
+        fonte = pygame.font.SysFont(None, 55)
+        mensagem = fonte.render("Jogo da Cobrinha em Python", True, branco)
+        tela.blit(mensagem, [largura / 6, altura / 3])
+
+        pygame.display.update()
+        relogio.tick(15)
